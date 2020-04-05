9b9c1a3191cea3ff8240461e749b691f98314627
Sabbir Ahmed <45328851+sabbir030@users.noreply.github.com>
2020-04-05 18:49:01 +0600

    Mouse click Event added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(e);
+
   if (!audio) return; //stop the function from running all together
   audio.currentTime = 0; //rewind to the start
   audio.play();
@@ -10,10 +12,38 @@ function playSound(e) {
 function removeTransition(e) {
   if (e.propertyName !== "transform") return; // skip it if it's not a transform
   this.classList.remove("playing");
-  console.log(this);
+  // console.log(this);
 }
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", playSound);
 
+
+
+
+/* window.addEventListener('click', function (e) { 
+  if (e.target.innerText.length == 1) {
+  const asciiCode = e.target.innerText.charCodeAt(0); // get the text and convert it to ascii
+  const audio = document.querySelector(`audio[data-key="${asciiCode}"]`);
+  const key = document.querySelector(`.key[data-key="${asciiCode}"]`);
+  if (!audio) return; //stop the function from running all together
+  audio.currentTime = 0; //rewind to the start
+  audio.play();
+  key.classList.add("playing"); }
+}); 
+
+ */
+
+
+ window.addEventListener('click', function (e) { 
+  const asciiCode = e.target.innerText.charCodeAt(0); // get the text and convert it to ascii
+  const audio = document.querySelector(`audio[data-key="${asciiCode}"]`);
+  const key = document.querySelector(`.key[data-key="${asciiCode}"]`);
+  if (!audio) return; //stop the function from running all together
+  if(e.target.tagName !== "KBD") return;
+  audio.currentTime = 0; //rewind to the start
+  audio.play();
+  key.classList.add("playing"); 
+});
+ 
